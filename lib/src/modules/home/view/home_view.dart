f25360dcc22800ca0b3ccbd05372120c735d428a
Santo Shakil <shakil.mezbah@gmail.com>
2022-07-01 21:45:55 +0600

    Theme Switcher

--- lib/src/modules/home/view/home_view.dart
+++ lib/src/modules/home/view/home_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../../clock/view/clock.dart';
 import '../../paints/circle/view/circle.dart';
+import '../../setting/provider/config/app_config_db.dart';
+import '../../setting/provider/theme/theme_db.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -9,7 +11,15 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('CustomPainter')),
+      appBar: AppBar(
+        title: const Text('Clock'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.color_lens_rounded),
+            onPressed: () => changeTheme(appConfig.themeIndex == 0 ? 1 : 0),
+          ),
+        ],
+      ),
       body: const Clock(),
     );
   }
